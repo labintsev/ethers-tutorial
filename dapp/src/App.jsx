6bbeac7861004c16956afee6c1ecd582c449cbe3
Labintsev A I <andrej.labintsev@yandex.ru>
2024-11-21 10:00:24 +0300

    Update cors comments

--- dapp/src/App.jsx
+++ dapp/src/App.jsx
@@ -1,11 +1,13 @@
 import { ethers } from "ethers";
 import './App.css'
 
+// case 1 - block with cors error
 // const providerUrl = 'https://sepolia-eth.web3gate.ru/18dc5b64-a810-4bd0-80dc-ffa4657b168e';
-
 // const provider = new ethers.JsonRpcProvider(providerUrl);
-const provider = new ethers.JsonRpcProvider("https://shape-mainnet.g.alchemy.com/v2/5gSTs0ulBGLemVyCcVRXT0FR_aM-Rwyq");
 
+// case 2 - works fine
+const provider = new ethers.JsonRpcProvider("https://shape-mainnet.g.alchemy.com/v2/5gSTs0ulBGLemVyCcVRXT0FR_aM-Rwyq");
+ 
 const network = await provider.getNetwork();
   
 function App() {
